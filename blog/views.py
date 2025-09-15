712d46427b378d1c6249d91010f1699fa4adb552
Birat <itsbirataryal+github@gmail.com>
2025-09-15 22:52:25 +0545

    refactor: update views and URL routing to include default route

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+
 from .models import Post
+from django.http import HttpResponse
 
 
 def post_list(request):
@@ -7,3 +9,7 @@ def post_list(request):
     posts = Post.objects.all().order_by("-created_at")
     print("This is the posts:", posts)
     return render(request, "blog/post_list.html", {"posts": posts})
+
+
+def default_route(request):
+    return HttpResponse("Hello, this is the home screen")
--- myblog/urls.py
+++ myblog/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import post_list
+from blog.views import default_route
 
 # Everything starting with this goes to the specific file or directory
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", post_list),
+    path("", default_route ),
     path("blog/", include("blog.urls")),
     # everything starting with /blog/ goes into blog/urls.p
 ]

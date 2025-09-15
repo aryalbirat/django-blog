757524106b4f30e22375a5adae40154a6af2d2a5
Birat <itsbirataryal+github@gmail.com>
2025-09-15 22:28:18 +0545

    update URL patterns for correct routing

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,7 @@ from .models import Post
 
 
 def post_list(request):
+    print("This is the request:", request)
     posts = Post.objects.all().order_by("-created_at")
+    print("This is the posts:", posts)
     return render(request, "blog/post_list.html", {"posts": posts})
--- myblog/urls.py
+++ myblog/urls.py
@@ -1,11 +1,11 @@
-
-
 from django.contrib import admin
 from django.urls import path, include
 from blog.views import post_list
 
+# Everything starting with this goes to the specific file or directory
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("hello/", post_list),
-    path('blog/', include('blog.urls'))
+    path("", post_list),
+    path("blog/", include("blog.urls")),
+    # everything starting with /blog/ goes into blog/urls.p
 ]
